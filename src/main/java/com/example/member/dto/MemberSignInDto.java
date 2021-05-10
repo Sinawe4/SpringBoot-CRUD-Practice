01ec090dcb92fd83a09adf84168bb746fb715934
JeongYongWoo <aba1430@naver.com>
2021-05-10 19:45:35 +0900

    [FIX] MemberSignInDto

--- src/main/java/com/example/member/dto/MemberSignInDto.java
+++ src/main/java/com/example/member/dto/MemberSignInDto.java
@@ -1,16 +1,19 @@
 package com.example.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class MemberSignInDto {
-    //@NotBlank(message = "name should be valid")
+    @NotBlank(message = "name should be valid")
     private String name;
 
-    //@NotBlank(message = "password should be valid")
+    @NotBlank(message = "password should be valid")
     private String password;
 }
